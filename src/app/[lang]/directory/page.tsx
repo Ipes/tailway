a11d63aacefbd5219c07142b02964b568549bdcc
Toon <toon.lambrechts@gmail.com>
2025-01-11 17:04:23 +0100

    updated Pageprops to props to avoid conflict with internal types

--- src/app/[lang]/directory/page.tsx
+++ src/app/[lang]/directory/page.tsx
@@ -3,19 +3,20 @@ import { getDictionary } from '@/lib/dictionary'
 import DirectoryPage from '@/components/DirectoryPage'
 import { Locale } from '@/config/i18n.config'
 
-interface PageProps {
+type Props = {
   params: {
     lang: Locale
   }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default async function Page({ params }: PageProps) {
-  const dict = await getDictionary(params.lang)
+export default async function Page(props: Props) {
+  const dict = await getDictionary(props.params.lang)
   
   return (
     <DirectoryPage 
       dict={dict} 
-      lang={params.lang}
+      lang={props.params.lang}
     />
   )
 }
