d52c1ef511c17c4f36fe2765dd97b847be2fb173
Toon <toon.lambrechts@gmail.com>
2025-01-11 22:12:29 +0100

    update to Next JS 15 p2

--- src/app/[lang]/directory/page.tsx
+++ src/app/[lang]/directory/page.tsx
@@ -2,16 +2,17 @@
 import { getDictionary } from '@/lib/dictionary'
 import DirectoryPage from '@/components/DirectoryPage'
 import { Locale } from '@/config/i18n.config'
+import { use } from 'react'
 
-export default async function Page({
+export default function Page({
   params,
   searchParams,
 }: {
   params: Promise<{ lang: Locale }>;
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const { lang } = await params;
-  const dict = await getDictionary(lang)
+  const { lang } = use(params);
+  const dict = use(getDictionary(lang))
   
   return (
     <DirectoryPage 
